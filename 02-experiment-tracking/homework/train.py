33ba268cfb0b4f7c08a6f0cc1b92017c76992221
Dario Dang <ankhuong9999@gmail.com>
2025-05-09 08:30:35 +0000

    Update train.py script

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -1,10 +1,21 @@
 import os
 import pickle
 import click
+import mlflow 
+import mlflow.sklearn
+
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+# Set the tracking URI to the same one used in your UI
+mlflow.set_tracking_uri("http://127.0.0.1:5002")  
+
+# Create or set the experiment
+mlflow.set_experiment("nyc-taxi-experiment")
+
+# Enable autologging
+mlflow.sklearn.autolog()
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -21,12 +32,20 @@ def run_train(data_path: str):
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
-
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+    
+    #Run the MLFlow
+    with mlflow.start_run(run_name="RandomForest"):
+        mlflow.set_tag("developer","Dario")
+        mlflow.set_tag("model", "RandomForest")
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+        
+        # Calculate RMSE
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        
+        # Show the result 
+        print(f"RMSE: {rmse:.4f}")
 
 
 if __name__ == '__main__':
