01a14c7561cd35a820652a31ad4f605494fcd0ae
DarioDang <ankhuong9999@gmail.com>
2025-06-28 13:11:05 +1200

    Update bash script to run the integrate tests

--- 06-best-practices/integration-tests/run.sh
+++ 06-best-practices/integration-tests/run.sh
@@ -4,11 +4,16 @@
 
 cd "$(dirname "$0")" # This make sure always navigate to the script directory
 
-LOCAL_TAG=$(date +%Y-%m-%d_%H-%M)
-export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
-export PREDICTIONS_STREAM_NAME="ride_predictions"
+if ["${LOCAL_IMAGE_NAME}" == ""]; then
+    LOCAL_TAG=$(date +%Y-%m-%d_%H-%M)
+    echo "LOCAL_IMAGE_NAME is not set, building the image with tag ${LOCAL_IMAGE_NAME}"
+    export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
+    docker build -t ${LOCAL_IMAGE_NAME} ../code
+else 
+    echo "no need to build image ${LOCAL_IMAGE_NAME}"
+fi
 
-docker build -t ${LOCAL_IMAGE_NAME} ../code
+export PREDICTIONS_STREAM_NAME="ride_predictions"
 
 docker-compose up -d  
 
